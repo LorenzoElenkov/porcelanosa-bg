7e58cd384a87cb9e8974b91eaf3b84a75672d595
LorenzoElenkov <thevalorbg@gmail.com>
2025-02-09 20:58:54 +0200

    Updates

---
+++ _next/static/chunks/app/catalogues/urbatek/page-4df026cd756e4689.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[112,121,241,487,518,655,869,893,947],{4685:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7970,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[970,441,517,358],()=>s(4685)),_N_E=e.O()}]);
